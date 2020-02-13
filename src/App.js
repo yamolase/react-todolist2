ed5766f3af83097e6dc64d47edb5eb997e6fdd3f
yamolase <60477158+yamolase@users.noreply.github.com>
2020-02-14 05:02:49 +0700

    migrasi htnl css ke react

--- src/App.js
+++ src/App.js
@@ -3,9 +3,46 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-btn main-black-color">add</button>
+          <h1 className="header-title">Todo list</h1>
+          <button className="header-btn main-red-color">Clear</button>
+        </section>
+        <section className="add">
+          <form className="add-form">
+            <input type="text" className="add-input" />
+            <butto className="add-btn main-black-color">Add</butto>
+          </form>
+        </section>
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
