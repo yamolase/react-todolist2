3e894c3aad466cd38280406b78a9290f52f72396
yamolase <60477158+yamolase@users.noreply.github.com>
2020-02-16 01:33:40 +0700

    membuat paper component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
 import React from "react";
 import "./styles.css";
 
+import Paper from "./components/paper";
+
 export default function App() {
   return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button className="header-btn main-black-color">add</button>
-          <h1 className="header-title">Todo list</h1>
-          <button className="header-btn main-red-color">Clear</button>
-        </section>
-        <section className="add">
-          <form className="add-form">
-            <input type="text" className="add-input" />
-            <butto className="add-btn main-black-color">Add</butto>
-          </form>
-        </section>
-        <section className="todos">
-          <div className="todo">
-            <span className="todo-text">learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">learning React</span>
-          </div>
-        </section>
-      </div>
-    </div>
+    <Paper>
+      <section className="header">
+        <button className="header-btn main-black-color">add</button>
+        <h1 className="header-title">Todo list</h1>
+        <button className="header-btn main-red-color">Clear</button>
+      </section>
+      <section className="add">
+        <form className="add-form">
+          <input type="text" className="add-input" />
+          <butto className="add-btn main-black-color">Add</butto>
+        </form>
+      </section>
+      <section className="todos">
+        <div className="todo">
+          <span className="todo-text">learning React</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">learning React</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">learning React</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">learning React</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">learning React</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">learning React</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">learning React</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">learning React</span>
+        </div>
+      </section>
+    </Paper>
   );
 }
---
+++ src/components/paper.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+const Paper = ({ children }) => {
+  return (
+    <div className="container">
+      <div className="frame">{children}</div>
+    </div>
+  );
+};
+
+Paper.proptypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
+
+export default Paper;
